6f6f6d8464f5e1e8c2b8448ef6e3333a8cd500df
Truong Thanh Tung <ttungbmt@gmail.com>
2019-07-12 19:33:09 +0700

    Update uniqid

--- src/uniqid.js
+++ src/uniqid.js
@@ -4,4 +4,7 @@ function uniqid(prefix, suffix) {
     return (prefix || '') + shortid.generate() || (suffix || '');
 }
 
+uniqid.isValid = (id) => shortid.isValid(id)
+uniqid.seed = (int) => shortid.seed(int)
+
 export default uniqid
